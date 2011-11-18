aad5da04b729060a08903d4c9172ac31f6f49fdb
Roger Hughes <info@captaindebug.com>
2011-11-18 17:55:48 +0000

    Unit Test Simplification

--- address/src/test/java/com/captaindebug/address/ClassicAddressServiceWithStubTest.java
+++ address/src/test/java/com/captaindebug/address/ClassicAddressServiceWithStubTest.java
@@ -5,10 +5,13 @@ package com.captaindebug.address;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * Sample Unit test using a Stub
+ * 
  * @author Roger
  * 
  */
@@ -16,12 +19,10 @@ public class ClassicAddressServiceWithStubTest {
 
 	private AddressService instance;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@Before
 	public void setUp() throws Exception {
-
+		/* Create the object to test */
+		/* Setup data that's used by ALL tests in this class */
 		instance = new AddressService();
 	}
 
@@ -32,16 +33,27 @@ public class ClassicAddressServiceWithStubTest {
 	@Test
 	public void testFindAddressWithStub() {
 
+		/* Setup the test data - stuff that's specific to this test */
 		Address expectedAddress = new Address(1, "15 My Street", "My Town",
 				"POSTCODE", "My Country");
 		instance.setAddressDao(new StubAddressDao(expectedAddress));
 
+		/* Run the test */
 		Address result = instance.findAddress(1);
 
+		/* Assert the results */
 		assertEquals(expectedAddress.getId(), result.getId());
 		assertEquals(expectedAddress.getStreet(), result.getStreet());
 		assertEquals(expectedAddress.getTown(), result.getTown());
 		assertEquals(expectedAddress.getPostCode(), result.getPostCode());
 		assertEquals(expectedAddress.getCountry(), result.getCountry());
+	}
+
+	@After
+	public void tearDown() {
+		/*
+		 * Clear up to ensure all tests in the class are isolated from each
+		 * other.
+		 */
 	}
 }
