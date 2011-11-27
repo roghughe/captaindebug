07ad35a33836e8d543aee08195bcf95b116eeb84
Roger <me@rogerhughes.co.uk>
2011-11-27 20:09:21 +0000

    Stub using inheritance

---
+++ address/src/test/java/com/captaindebug/siteproperties/LegacyAddressServiceUsingInheritanceTest.java
@@ -0,0 +1,83 @@
+package com.captaindebug.siteproperties;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.captaindebug.address.Address;
+import com.captaindebug.address.StubAddressDao;
+
+/**
+ * Stub based test for LegacyAddressService
+ * 
+ * @author Roger
+ * 
+ *         Created 9:13:49 PM Nov 24, 2011
+ * 
+ */
+public class LegacyAddressServiceUsingInheritanceTest {
+
+	private StubAddressDao addressDao;
+
+	private StubSitePropertiesUsingInheritance stubProperties;
+
+	private LegacyAddressService instance;
+
+	@Before
+	public void setUp() {
+		instance = new LegacyAddressService();
+
+		stubProperties = new StubSitePropertiesUsingInheritance();
+		instance.setPropertiesManager(stubProperties);
+	}
+
+	@Test
+	public void testAddressSiteProperties_AddressServiceDisabled() {
+
+		/* Set up the AddressDAO Stubb for this test */
+		Address address = new Address(1, "15 My Street", "My Town", "POSTCODE", "My Country");
+		addressDao = new StubAddressDao(address);
+		instance.setAddressDao(addressDao);
+
+		stubProperties.setProperty("address.enabled", "false");
+
+		Address expected = Address.INVALID_ADDRESS;
+		Address result = instance.findAddress(1);
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void testAddressSiteProperties_AddressServiceEnabled() {
+
+		/* Set up the AddressDAO Stubb for this test */
+		Address address = new Address(1, "15 My Street", "My Town", "POSTCODE", "My Country");
+		addressDao = new StubAddressDao(address);
+		instance.setAddressDao(addressDao);
+
+		stubProperties.setProperty("address.enabled", "true");
+
+		Address result = instance.findAddress(1);
+
+		assertEquals(address, result);
+	}
+
+	public class StubSitePropertiesUsingInheritance extends SitePropertiesManager {
+
+		private final Map<String, String> propMap = new HashMap<String, String>();
+
+		public void setProperty(String key, String value) {
+			propMap.put(key, value);
+		}
+
+		@Override
+		public String findProperty(String propertyName) {
+			return propMap.get(propertyName);
+		}
+	}
+
+}
