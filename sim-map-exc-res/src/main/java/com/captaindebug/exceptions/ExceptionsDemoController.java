df4928979a20450d2808fd6d6ac85bbdd74b0518
Roger Hughes <info@captaindebug.com>
2012-03-06 12:48:12 +0000

    ??? Can't Remember

--- sim-map-exc-res/src/main/java/com/captaindebug/exceptions/ExceptionsDemoController.java
+++ sim-map-exc-res/src/main/java/com/captaindebug/exceptions/ExceptionsDemoController.java
@@ -1,7 +1,9 @@
 package com.captaindebug.exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Locale;
+import java.util.zip.DataFormatException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,13 +15,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ExceptionsDemoController {
 
-	private static final Logger logger = LoggerFactory.getLogger(ExceptionsDemoController.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(ExceptionsDemoController.class);
 
 	/**
 	 * Whoops, throw an IOException
 	 */
 	@RequestMapping(value = "/ioexception", method = RequestMethod.GET)
-	public String throwAnIOException(Locale locale, Model model) throws IOException {
+	public String throwAnIOException(Locale locale, Model model)
+			throws IOException {
 
 		logger.info("This will throw an IOException");
 
@@ -33,7 +37,25 @@ public class ExceptionsDemoController {
 	}
 
 	/**
-	 * Whoops, throw an IOException
+	 * Whoops, throw a FileNotFoundException
+	 */
+	@RequestMapping(value = "/filenotfoundexception", method = RequestMethod.GET)
+	public String throwAFileNotFoundException(Locale locale, Model model)
+			throws IOException {
+
+		logger.info("This will throw a FileNotFoundException");
+
+		boolean throwException = true;
+
+		if (throwException) {
+			throw new FileNotFoundException("This is my FileNotFoundException");
+		}
+
+		return "home";
+	}
+
+	/**
+	 * Whoops, throw an Exception
 	 */
 	@RequestMapping(value = "/exception", method = RequestMethod.GET)
 	public String throwAnException(Locale locale, Model model) throws Exception {
@@ -43,7 +65,7 @@ public class ExceptionsDemoController {
 		boolean throwException = true;
 
 		if (throwException) {
-			throw new Exception("This is my Exception");
+			throw new DataFormatException("This is my Exception");
 		}
 
 		return "home";
