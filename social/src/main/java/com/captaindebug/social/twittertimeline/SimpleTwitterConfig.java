ce33615b3581dc0d35693e46235b3cca422b88ec
Roger Hughes <me@rogerhughes.co.uk>
2012-06-25 14:23:34 +0100

    A Comment change

--- social/src/main/java/com/captaindebug/social/twittertimeline/SimpleTwitterConfig.java
+++ social/src/main/java/com/captaindebug/social/twittertimeline/SimpleTwitterConfig.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2011 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
  */
 package com.captaindebug.social.twittertimeline;
 
@@ -38,8 +36,8 @@ public class SimpleTwitterConfig {
 	}
 
 	/**
-	 * A proxy to a request-scoped object representing the simplest Twitter API
-	 * - one that doesn't need any authorization
+	 * A proxy to a request-scoped object representing the simplest Twitter API - one that
+	 * doesn't need any authorization
 	 */
 	@Bean
 	@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
