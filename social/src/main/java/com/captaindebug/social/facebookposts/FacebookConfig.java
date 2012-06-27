9db5afb5b5bd6b6b72a07944ae038fac5c7fa47d
Roger Hughes <me@rogerhughes.co.uk>
2012-06-27 20:51:55 +0100

    Facebook work not complete

--- social/src/main/java/com/captaindebug/social/facebookposts/FacebookConfig.java
+++ social/src/main/java/com/captaindebug/social/facebookposts/FacebookConfig.java
@@ -2,8 +2,11 @@ package com.captaindebug.social.facebookposts;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.NotConnectedException;
 import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 
 @Configuration
@@ -28,14 +31,10 @@ public class FacebookConfig {
 	 * @throws NotConnectedException
 	 *             if the user is not connected to facebook.
 	 */
+	@Bean
+	@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
+	public Facebook facebook() {
 
-	/*
-	 * TODO You are here $$$$$
-	 * 
-	 * @Bean
-	 * 
-	 * @Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES) public Facebook
-	 * facebook() { return
-	 * connectionRepository().getPrimaryConnection(Facebook.class).getApi(); }
-	 */
+		return connectionRepository().getPrimaryConnection(Facebook.class).getApi();
+	}
 }
