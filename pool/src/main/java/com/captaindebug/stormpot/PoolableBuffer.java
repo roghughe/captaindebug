475776c9f9d5fdcfee59a41b02b9b247810e6108
Roger Hughes <info@captaindebug.com>
2018-11-24 18:48:41 +0000

    Pool Started

--- pool/src/main/java/com/captaindebug/stormpot/PoolableBuffer.java
+++ pool/src/main/java/com/captaindebug/stormpot/PoolableBuffer.java
@@ -3,22 +3,18 @@ package com.captaindebug.stormpot;
 import stormpot.Poolable;
 import stormpot.Slot;
 
-public class PoolableBuffer implements Poolable  {
+public class PoolableBuffer implements Poolable {
 
-	
-	  private final Slot slot;
-	  
-	  public PoolableBuffer(Slot slot) {
-	    this.slot = slot;
-	  }
-	
-	
-	@Override
-	public void release() {
-	    slot.release(this);
+	private final Slot slot;
+
+	public PoolableBuffer(Slot slot) {
+		this.slot = slot;
 	}
 
+	@Override
+	public void release() {
+		slot.release(this);
+	}
 
-	
 	// Implement the ByeBuffer methods
 }
